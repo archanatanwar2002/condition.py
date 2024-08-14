05f9687dea15170255d257aa2a0033246d31f1d5
archanatanwar2002 <archanatanwar2002@gmail.com>
2024-08-14 19:21:24 +0530

    Add files via upload

---
+++ condition.py
@@ -0,0 +1,17 @@
+#age = 23
+#if(age>=23):
+
+   # print("can vote")
+#else:
+ #   print("can not vote")
+mark = 34
+marks = input("enter the marks:")
+if(marks>10):
+    grade = "a"
+elif(marks<=100 ):
+    grade = "b"
+elif(marks<=74):
+    grade = "d"
+else:
+    grade = "f"
+    print("grade the student  ",grade)
